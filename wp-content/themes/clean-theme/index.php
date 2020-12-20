82e3050ab5ac0083263c0984107d91525de9600c
Vladyslav Vakaliuk <vladyslav_vakaliuk@epam.com>
2020-12-20 23:40:46 +0200

    install plugin

--- wp-content/themes/clean-theme/index.php
+++ wp-content/themes/clean-theme/index.php
@@ -3,41 +3,20 @@ get_header();
 ?>
 <div id="fh5co-portfolio">
 	<?php
-	$query = new WP_Query(
-		[
-			'category_name' => get_theme_mod('clean_home_category')
-		]
-	);
-	?>
 
+	if(have_posts()):
 
-	<?php if ( $query->have_posts() ) : $i = 1;
-		while ( $query->have_posts() ) : $query->the_post(); ?>
-			<?php
-			if ( has_post_thumbnail() ) {
-				$img_url = get_the_post_thumbnail_url();
-			} else {
-				$img_url = 'https://picsum.photos/1280/864';
-			}
-			?>
-			<div class="fh5co-portfolio-item <?php if ( $i % 2 == 0 ) {
-				echo 'fh5co-img-right';
-			} ?>">
-				<div class="fh5co-portfolio-figure animate-box"
-					 style="background-image: url(<?php echo $img_url; ?>);"></div>
-				<div class="fh5co-portfolio-description">
-					<h2><?php the_title(); ?></h2>
-					<?php the_content( '' ); ?>
-					<p><a href="<?php the_permalink(); ?>"
-						  class="btn btn-primary"><?php _e( 'Read more', 'clean' ); ?></a></p>
-				</div>
-			</div>
+		while (have_posts()):
+			the_post();
 
-			<?php $i ++; endwhile; ?>
-	<?php else: ?>
-		Post is not exists
-	<?php endif; ?>
-	<?php wp_reset_postdata(); ?>
+			the_title();
+			the_content();
+			the_permalink();
+
+		endwhile;
+
+	endif;
+?>
 </div>
 <?php
 get_footer();
